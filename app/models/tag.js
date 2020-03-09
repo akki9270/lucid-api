b40f99fc9e7d0207386ecc019ce0eb7f97cf11e6
chandrakant rana <chandrakant@jumpbyte.com>
2020-03-09 10:11:04 +0545

    changed in tag module - changed patient data - changed tag data

--- app/models/tag.js
+++ app/models/tag.js
@@ -2,7 +2,8 @@
 module.exports = function (Sequelize, Types) {
     let Tag = Sequelize.define('Tag', {
         tag_code: { type: Types.STRING, primaryKey: true },
-        tag_name: { type: Types.STRING }
+        tag_name: { type: Types.STRING },
+        tag_color: { type: Types.STRING },
     }, {
         tableName: 'Tag',
         modelName: 'Tag',
